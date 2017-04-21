00fd526eb3eef10d87dccf6b7fe7a9d99dd5c325
vinoth <vinoth@linkstreet.in>
2017-04-21 20:15:11 +0530

    [BUGFIX #137]The voice search icon is not displaying for some reason

--- library/src/main/java/com/miguelcatalan/materialsearchview/MaterialSearchView.java
+++ library/src/main/java/com/miguelcatalan/materialsearchview/MaterialSearchView.java
@@ -279,7 +279,7 @@ public class MaterialSearchView extends FrameLayout implements Filter.FilterList
         PackageManager pm = getContext().getPackageManager();
         List<ResolveInfo> activities = pm.queryIntentActivities(
                 new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
-        return activities.size() == 0;
+        return activities.size() != 0;
     }
 
     public void hideKeyboard(View view) {
